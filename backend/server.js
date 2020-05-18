4c029d0352994c64191cfaeed4801baf48178996
vontell <vontell@mit.edu>
2020-05-18 19:32:41 -0400

    Heroku attempt 6

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,15 @@ app.use(function (err, req, res, next) {
   next()
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  });
+}
+
 app.alreadyStarted = false;
 
 app.server = app.listen(config.serverPort, async () => {
