bb1337d4d787c9c00420c2b8a0771be295efc264
vontell <vontell@mit.edu>
2020-08-20 00:36:17 -0400

    Better storybook

--- frontend/src/components/TaskV3.stories.js
+++ frontend/src/components/TaskV3.stories.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, text, date, boolean, number, color } from '@storybook/addon-knobs/react';
 
 
 import TaskV3 from './TaskV3';
 import Taskk from '../Models'
 
 export default {
-  component: TaskV3,
+  //component: TaskV3,
   title: 'Task',
-  decorators: [withKnobs],
   // Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/,
+  argTypes: {
+    priority: {control: { type: 'range', min: 0, max: 2, step: 1 }},
+    progress: {control: { type: 'range', min: 0, max: 1, step: .01 }},
+    name: { control: 'text' },
+    category: { control: 'text' },
+    dueDate: { control: 'date' },
+    notes: { control: {type: 'text'}},
+    completed: { control: 'boolean' },
+    categoryColor: { control: 'color' }
+  },
 };
-
-const groupId = 'GROUP-ID1';
 
 export const exampleTaskData = { 
     "_id": "5eefbebb5baaa3b7f06e05bd", 
@@ -25,7 +31,7 @@ export const exampleTaskData = {
     "parent": "5eefbeb55baaa3b7f06e05bc", 
     "priority": 2, 
     "dueDate": new Date('Jan 25 2017'),
-    "category": "My Category",
+    "category": "My Categories",
     "categoryColor": "#41B768"
   }
 
@@ -34,33 +40,38 @@ export const actionsData = {
   onArchiveTask: action('onArchiveTask'),
 };
 
-export const Configurable = () => {
-  let newTaskData = Object.assign({}, exampleTaskData);
+const Template = (args) => {
+  var newTaskData = Object.assign({}, exampleTaskData);
+  newTaskData = Object.assign(newTaskData, args);
   let task = new Taskk(newTaskData);
 
-  task.dueDate = new Date(date('Task Date', exampleTaskData.dueDate, groupId))
-  task.name = text('Task Title', exampleTaskData.name, groupId)
-  task.notes = text('Task Notes', exampleTaskData.notes, groupId)
-  task.completed = boolean('Task Completed', exampleTaskData.completed, groupId)
-  task.priority = number('Task Priority', exampleTaskData.priority, {
-    range: true,
-    min: 0,
-    max: 2,
-    step: 1,
-  }, groupId)
-
-  let progress = number('Task Progress', 0, {
-    range: true,
-    min: 0,
-    max: 1,
-    step: 0.01,
-  }, groupId)
-  task.category = text('Task Category', exampleTaskData.category, groupId);
-  task.categoryColor = color('Task Category Color', exampleTaskData.categoryColor, groupId)
-
-  console.log(task)
-  return (<TaskV3 task={task} progress={progress} {...actionsData} />)
+  console.log(task);
+  return (<TaskV3 task={task} progress={args.progress} {...actionsData} />)
 }
+
+export const Default = Template.bind({})
+
+Default.args = {
+  dueDate: exampleTaskData.dueDate,
+  name: exampleTaskData.name,
+  notes: exampleTaskData.notes,
+  completed: exampleTaskData.completed,
+  priority: exampleTaskData.priority,
+  progress: 0.0,
+  category: exampleTaskData.category,
+  categoryColor: exampleTaskData.categoryColor
+}
+
+// Configurable.argTypes = {
+//   priority: { type: 'range', min: 0, max: 2, step: 1 },
+//   progress: { type: 'range', min: 0, max: 1, step: .01 },
+//   name: { control: 'text' },
+//   category: { control: 'text' },
+//   dueDate: { control: 'date' },
+//   notes: { control: 'text' },
+//   compeleted: { control: 'boolean' },
+//   categoryColor: { control: 'color' }
+// }
 
 // export const Pinned = () => (<TaskV3 task={exampleTaskData} {...actionsData} />);
 
